ebd7a7473d902c198c8a6ba58714f2e7b200720a
wyd600 <49527934+wyd600@users.noreply.github.com>
2019-05-05 20:17:32 +0800

    添加项目文件。

---
+++ WPF+MVVM+EF/Class2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_EF.ViewModels
+{
+    public class AddOrEditViewModel : ViewModelBase
+
+
+    {
+
+
+        private StudentEntity _currentStudentEntity;
+        public StudentEntity CurrentStudentEntity
+        {
+            get { return _currentStudentEntity; }
+            set
+            {
+                if (_currentStudentEntity != value)
+                {
+                    _currentStudentEntity = value;
+                    if (_currentStudentEntity.StudentSex == 0)
+                    {
+                        IsChecked = true;
+                    }
+                    else
+                    {
+                        IsChecked = false;
+                    }
+
+                    this.OnPropertyChanged(r => r.CurrentStudentEntity);
+                }
+            }
+        }
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                if (_isChecked != value)
+                {
+                    _isChecked = value;
+                    this.OnPropertyChanged(r => r.IsChecked);
+                }
+            }
+        }
+
+        public StudentDal StudentDal { get; set; }
+
+        private bool _isAdd = false;
+
+        public bool IsAdd
+        {
+            get { return _isAdd; }
+            set
+            {
+                if (_isAdd != value)
+                {
+                    _isAdd = value;
+                    this.OnPropertyChanged(r => r.IsAdd);
+                }
+            }
+        }
+        public ICommand SaveCommand { get; set; }
+
+        public AddOrEditViewModel()
+        {
+            SaveCommand = new DelegateCommand(Save);
+        }
+
+        private void Save()
+        {
+            StudentEntity student = new StudentEntity();
+            student.StudentName = CurrentStudentEntity.StudentName;
+            student.StudentAge = CurrentStudentEntity.StudentAge;
+            student.StudentSex = IsChecked ? 0 : 1;
+            student.StudentAddress = CurrentStudentEntity.StudentAddress;
+            if (IsAdd)
+            {
+                StudentDal.Insert(student);
+            }
+            else
+            {
+                student.StudentId = CurrentStudentEntity.StudentId;
+                StudentDal.Update(student);
+            }
+        }
+    }
+}
---
+++ WPF+MVVM+EF/Dal/CRUD.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_EF.Dal
+{
+    public class CRUD
+    {
+        //创建上下文对象
+        static Entities context = new Entities();
+
+        //获取数据库中的所有数据
+        public static List<Student> GetData()
+        {
+            List<Student> result = new List<Student>();
+            var list = from u in context.Student
+                       select u;
+            foreach (Student item in list)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+        //插入一条数据
+        public static void InsertAData(Student str)
+        {
+            Student student = AddData(str);
+            context.Entry<Student>(student).State = System.Data.Entity.EntityState.Added;
+            context.SaveChanges();
+        }
+
+        private static Student AddData(Student str)
+        {
+            Student student = new Student();
+            student.StudentId = str.StudentId;
+            student.StudentName = str.StudentName;
+            student.StudentAge = str.StudentAge;
+            student.StudentSex = str.StudentSex;
+            student.StudentAddress = str.StudentAddress;
+            return student;
+        }
+
+        //删除一条数据
+        public static void DeletedAData(Student str)
+        {
+            Student student = new Student();
+            if (str.StudentId != 0)
+            {
+
+            }
+        }
+
+        //修改一条数据
+        public static void Modify()
+        {
+        }
+
+        //查找一条数据
+        public static Student SelectAData()
+        {
+
+        }
+    }
+}
